270231dee55a1af01dfa842bfb263128cd4abb70
Lena <lena@HbIOTOH>
2013-03-28 23:06:48 -0700

    initial commit

---
+++ ex2.py
@@ -0,0 +1,6 @@
+# A comment, this is so I can read the program later.
+# Anything after # is ignored by python.
+print "I could have code like this." # and the comment after is ignored
+# You can also use the comment to "disable" or comment ot a piece of code 
+# print "this wont run"
+print "this will run"
---
+++ ex3.py
@@ -0,0 +1,19 @@
+print "I will now count my chickens:"
+print "this is math", 7/4
+print "this is floating point math", 7.0/4.0
+
+print "Hens", 25.0 + 30.0 / 6.0 # add and divide chicken count
+print "Roosters", 100 - 25 * 3 % 4 # 25 times 3 (75) mod 4 
+
+print "Now I will count my eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "Is it true that 3 + 2 < 5 - 7?"
+print "what is 3 + 2?" , 3 + 2
+print "what is 5 - 7?" , 5 - 7
+print "Oh, that's why it's False."
+print "How about some more."
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?" , 5 <= -2
