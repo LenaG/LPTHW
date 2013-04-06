b52d0c541e43aadb6f9015d551ab47f6939b083f
Lena <lena@bigC.com>
2013-04-06 15:36:12 -0700

    added instructions to ex 8

--- ex8.py
+++ ex8.py
@@ -1,5 +1,7 @@
+# defined variable with placeholders
 formatter = "%r %r %r %r"
 
+# printed variable with diff meaning for placeholder, numbers, strings, defined boolean, variable itself inserted into placeholder, and strings again with commas to make shorter lines of code
 print formatter % (1, 2, 3, 4)
 print formatter % ("one", "two", "three", "four")
 print formatter % (True, False, False, True)
