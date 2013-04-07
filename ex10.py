b1e648e2173474ac9401488de746e40214ca012c
Lena <lena@bigC.com>
2013-04-06 20:29:39 -0700

    ex 10 pasha crazy shenanigans

--- ex10.py
+++ ex10.py
@@ -5,6 +5,8 @@ print 'I am 6\'2" tall.'
 # \t = tab, indent
 tabby_cat = "\tI'm tabbed in."
 # \n = new line
+import time 
+import sys
 persian_cat = "I'm split\non a line."
 # \\ = prints one \
 backslash_cat = "I'm \\ a \\ cat."
@@ -21,3 +23,12 @@ print tabby_cat
 print persian_cat
 print backslash_cat
 print fat_cat
+
+
+# pasha be crazy
+while True:
+ for i in ["/","-","|","\\","|"]:
+     time.sleep(.3)   
+     print "%s\r" % i,
+     sys.stdout.flush()
+
